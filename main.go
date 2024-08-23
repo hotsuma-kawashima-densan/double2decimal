4f838cfffd052534af268b02d336137fb0ec2736
832022 <hotsuma_kawashima@densan-s.co.jp>
2024-08-23 16:06:23 +0900

    update

--- main.go
+++ main.go
@@ -12,6 +12,18 @@ import (
 	"gorm.io/gorm"
 )
 
+type Result struct {
+	ID    int32           `gorm:"column:id;not null" json:"id"`
+	Input string          `gorm:"column:input;not null" json:"input"`
+	A     float64         `gorm:"column:A;not null" json:"A"`
+	B     decimal.Decimal `gorm:"column:B;not null" json:"B"`
+	B0    decimal.Decimal `gorm:"column:B0;not null" json:"B0"`
+	B1    decimal.Decimal `gorm:"column:B1;not null" json:"B1"`
+	B2    decimal.Decimal `gorm:"column:B2;not null" json:"B2"`
+	B3    decimal.Decimal `gorm:"column:B3;not null" json:"B3"`
+	B4    decimal.Decimal `gorm:"column:B4;not null" json:"B4"`
+}
+
 func main() {
 	dsn := "root:root@tcp(127.0.0.1:13306)/test?charset=utf8mb4&parseTime=True&loc=Local"
 
@@ -119,7 +131,7 @@ func main() {
 
 	bindParams["B4"] = data15.B4
 
-	var result []model.Temp
+	var result []Result
 	if err := db.Debug().Raw(sql, bindParams).Scan(&result).Error; err != nil {
 		fmt.Println("データ取得エラー:", err)
 		return
